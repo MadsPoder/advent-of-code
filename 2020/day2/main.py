c9154ae155471d97d383cfa936bc2bf7d5c6a018
MadsPoder <MadsPoder@users.noreply.github.com>
2020-12-06 14:01:03 +0100

    add day 2 part 2

--- 2020/day2/main.py
+++ 2020/day2/main.py
@@ -11,10 +11,19 @@ Password = namedtuple('PasswordWithPolicy', ['min', 'max', 'char', 'password'])
 with open ('input.txt') as fp:
     data = fp.readlines()
 
-# Unpack captured reges groups to named tuple
+# Unpack captured regex groups to named tuple
 passwords = [Password(*re.match(pattern, x).groups()) for x in data]
 
 # Valid passwords have occurences between min and max for the char in the password
 valid_passwords_part_1 = list(filter(lambda  x: int(x.min) <= x.password.count(x.char) <= int(x.max), passwords))
 
 print(len(valid_passwords_part_1))
+
+# Valid passwords for part 2 must have the char appear exatcly once in the positions defined in min or max
+# Similar to XOR. Subtract 1 from index as Toboggan Corporate Policies have no concept of "index zero"!
+valid_passwords_part_2 = list(filter(lambda  x: 
+    (x.password[int(x.min) - 1] == x.char) ^
+    (x.password[int(x.max) - 1] == x.char),
+    passwords))
+
+print(len(valid_passwords_part_2))
